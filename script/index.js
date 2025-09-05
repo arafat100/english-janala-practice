947f146aca638737f6df309c7e3d927013d4fc90
arafat100 <arafatr360@gmail.com>
2025-09-06 02:00:00 +0600

    alert

--- script/index.js
+++ script/index.js
@@ -4,6 +4,60 @@ const loadLessons = () => {
         .then((json => displayLesson(json.data)));
 };
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id} `;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLevelWord(data.data));
+
+}
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-conteiner")
+    wordContainer.innerHTML = "";
+
+
+    if (words.length == 0) {
+        wordContainer.innerHTML = ` 
+         <div class="text-center bg-sky-100 col-span-full">
+         <img class="mx-auto" src="./assets/alert-error.png"/>
+            <p class="text-xl font-medium text-gray-400 rounded-xl py-10 space-y-6 font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-xl text-4xl">নেক্সট Lesson এ যান</h2>
+        </div>
+        `;
+
+    }
+
+
+    // {
+    //     "id": 5,
+    //      "level": 1,
+    //     "word": "Eager",
+    //     "meaning": "আগ্রহী",
+    //     "pronunciation": "ইগার"
+    // }
+
+
+
+    words.forEach((word) => {
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = ` 
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <p class="font-semibold">Meaning /Pronounciation </p>
+            <div class="text-2xl font-medium font-bangla ">${word.meaning}/ ${word.pronunciation} </div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+             `;
+        wordContainer.append(card);
+
+    });
+}
+
+
 const displayLesson = (lessons) => {
     //1. get the container & empty
     const levelContainer = document.getElementById("level-container");
@@ -15,7 +69,7 @@ const displayLesson = (lessons) => {
         // 3. create Element
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = ` 
-                        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson- $(lesson.level_no)</button>
+                        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
                     
         `;
 
@@ -26,6 +80,6 @@ const displayLesson = (lessons) => {
 
 
 
-}
+};
 
 loadLessons();
